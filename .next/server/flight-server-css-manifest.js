951d95b6da9b5f1cbf09ca1ce78842573b494a25
Daniel Wang <danielwang1590@gmail.com>
2023-08-21 23:45:48 -0700

    add website

--- .next/server/flight-server-css-manifest.js
+++ .next/server/flight-server-css-manifest.js
@@ -1,10 +1,1 @@
-self.__RSC_CSS_MANIFEST={
-  "__entry_css_mods__": {
-    "/home/daniel/Documents/my-portfolio/app/page": [
-      "/home/daniel/Documents/my-portfolio/styles/globals.css"
-    ]
-  },
-  "/home/daniel/Documents/my-portfolio/app/layout.tsx": [
-    "/home/daniel/Documents/my-portfolio/styles/globals.css"
-  ]
-}
+self.__RSC_CSS_MANIFEST={"__entry_css_mods__":{"/home/daniel/Documents/my-portfolio/app/page":["/home/daniel/Documents/my-portfolio/styles/globals.css"]},"/home/daniel/Documents/my-portfolio/app/layout.tsx":["/home/daniel/Documents/my-portfolio/styles/globals.css"]}
